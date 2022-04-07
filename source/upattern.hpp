2ad90cb81a871715ff17dde29a7637ddfe101cac
AdamFull <logotipick@gmail.com>
2022-04-07 15:06:25 +0300

    fixed problem with singleton

--- source/upattern.hpp
+++ source/upattern.hpp
@@ -7,15 +7,15 @@ namespace utl
     template <class _Ty>
     class singleton : public non_copy_movable
     {
-        // template<> std::unique_ptr<_Ty> singleton<_Ty>::_instance{ nullptr };
+        // template<> unique_ptr<_Ty> singleton<_Ty>::_instance{ nullptr };
     protected:
-        static std::unique_ptr<_Ty> _instance;
+        static unique_ptr<_Ty> _instance;
 
     public:
-        static std::unique_ptr<_Ty> &getInstance()
+        static unique_ptr<_Ty> &getInstance()
         {
             if (!_instance)
-                _instance = make_unique<_Ty>(); //.reset(new _Ty());
+                _instance.reset(new _Ty());
             return _instance;
         }
     };
