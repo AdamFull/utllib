991672c2bcd3e23cf2f6531833fe51f87d88264b
AdamFull <logotipick@gmail.com>
2024-08-16 01:22:48 +0200

    Removed second part

--- source/hash.hpp
+++ source/hash.hpp
@@ -159,7 +159,6 @@ namespace utl
 		const int nblocks = len / 16;
 
 		uint64_t h1 = seed;
-		uint64_t h2 = seed;
 
 		for (int i = 0; i < nblocks; i++)
 		{
@@ -169,24 +168,56 @@ namespace utl
 			k1 *= c1; 
 			k1 = murmur64::rotl64(k1, 31); 
 			k1 *= c2; 
-			h1 ^= k1;
 
+			h1 ^= k1;
 			h1 = murmur64::rotl64(h1, 27); 
-			h1 += h2; 
 			h1 = h1 * 5 + 0x52dce729;
 
 			k2 *= c2; 
 			k2 = murmur64::rotl64(k2, 33); 
 			k2 *= c1; 
-			h2 ^= k2;
 
-			h2 = murmur64::rotl64(h2, 31); 
-			h2 += h1; 
-			h2 = h2 * 5 + 0x38495ab5;
+			h1 ^= k2;
+			h1 = murmur64::rotl64(h1, 31); 
+			h1 = h1 * 5 + 0x38495ab5;
 		}
 
-		// TODO: finish this
-		return 0ull;
+		uint64_t k1 = 0;
+		uint64_t k2 = 0;
+
+		const unsigned tail_start = len - (len % 16);
+		const char* tail = key + tail_start;
+
+		switch (len & 15)
+		{
+		case 15: k2 ^= static_cast<uint64_t>(tail[14]) << 48;
+		case 14: k2 ^= static_cast<uint64_t>(tail[13]) << 40;
+		case 13: k2 ^= static_cast<uint64_t>(tail[12]) << 32;
+		case 12: k2 ^= static_cast<uint64_t>(tail[11]) << 24;
+		case 11: k2 ^= static_cast<uint64_t>(tail[10]) << 16;
+		case 10: k2 ^= static_cast<uint64_t>(tail[9]) << 8;
+		case  9: k2 ^= static_cast<uint64_t>(tail[8]) << 0;
+			k2 *= c2; k2 = murmur64::rotl64(k2, 33); k2 *= c1; h1 ^= k2;
+
+		case  8: k1 ^= static_cast<uint64_t>(tail[7]) << 56;
+		case  7: k1 ^= static_cast<uint64_t>(tail[6]) << 48;
+		case  6: k1 ^= static_cast<uint64_t>(tail[5]) << 40;
+		case  5: k1 ^= static_cast<uint64_t>(tail[4]) << 32;
+		case  4: k1 ^= static_cast<uint64_t>(tail[3]) << 24;
+		case  3: k1 ^= static_cast<uint64_t>(tail[2]) << 16;
+		case  2: k1 ^= static_cast<uint64_t>(tail[1]) << 8;
+		case  1: k1 ^= static_cast<uint64_t>(tail[0]) << 0;
+			k1 *= c1; k1 = murmur64::rotl64(k1, 31); k1 *= c2; h1 ^= k1;
+		};
+
+		//----------
+		// finalization
+
+		h1 ^= len;
+
+		h1 = murmur64::fmix64(h1);
+
+		return h1;
 	}
 
 	inline constexpr u32 operator "" _fnv1a32(char const* s, size_t count) noexcept
